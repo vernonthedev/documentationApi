34977d13b286c901d7587c65e5c5d971bbb260af
vernonthedev <techjaja2@gmail.com>
2024-05-27 05:29:22 -0700

    #Feat: Added updating and patching of customers in the db thru api

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Requests\V1\StoreCustomerRequest;
-use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 use App\Models\Customer;
 use App\Http\Controllers\Controller;    
 use App\Http\Resources\V1\CustomerResource;
@@ -67,7 +67,7 @@ class CustomerController extends Controller
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
     }
 
     /**
